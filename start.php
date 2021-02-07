a3b23cd2b3d9e4ada7aa45a2d77d5447479518f9
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-02-07 22:50:44 +0700

    Update

--- start.php
+++ start.php
@@ -21,9 +21,9 @@ define('GITHUB_API_KEY', is_file($f = $dir . DS . 'key') ? file_get_contents($f)
 define('MIN_APACHE_VERSION', '2.4.0');
 define('MIN_PHP_VERSION', '7.1.0');
 
-define('THE_MECHA_VERSION', '2.5.3');
-define('THE_PANEL_VERSION', '2.5.1');
-define('THE_USER_VERSION', '1.12.1');
+define('THE_MECHA_VERSION', 'main');
+define('THE_PANEL_VERSION', 'main');
+define('THE_USER_VERSION', 'main');
 
 function fetch($url, $lot = null, $type = 'GET') {
     $headers = array('x-requested-with' => 'x-requested-with: CURL');
